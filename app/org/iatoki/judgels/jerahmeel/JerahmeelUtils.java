fa01eabc117070ad43e0f83d02771b533274eb86
Ivan Hendrajaya <ivan.hendrajaya@gmail.com>
2015-06-09 09:28:49 +0700

    remove unused code

--- app/org/iatoki/judgels/jerahmeel/JerahmeelUtils.java
+++ app/org/iatoki/judgels/jerahmeel/JerahmeelUtils.java
@@ -4,31 +4,13 @@ import com.google.common.collect.ImmutableList;
 import org.apache.commons.lang3.StringUtils;
 import play.mvc.Http;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 public class JerahmeelUtils {
 
     private JerahmeelUtils() {
         // prevent instantiation
-    }
-
-    public static String convertDateToString(Date date) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy, HH:mm");
-        return sdf.format(date);
-    }
-
-    public static Date convertStringToDate(String s) {
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy, HH:mm");
-            return sdf.parse(s);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
     public static List<String> getDefaultRoles() {
