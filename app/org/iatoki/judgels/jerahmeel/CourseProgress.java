16fa9a37878f9a8688c0263abc4e3111dfce793c
Ashar Fuadi <fushar@gmail.com>
2015-06-18 15:56:14 +0700

    ON_PROGRESS -> IN_PROGRESS

--- app/org/iatoki/judgels/jerahmeel/CourseProgress.java
+++ app/org/iatoki/judgels/jerahmeel/CourseProgress.java
@@ -3,6 +3,6 @@ package org.iatoki.judgels.jerahmeel;
 public enum CourseProgress {
     LOCKED,
     AVAILABLE,
-    ON_PROGRESS,
+    IN_PROGRESS,
     COMPLETED
 }
--- app/org/iatoki/judgels/jerahmeel/SessionProgress.java
+++ app/org/iatoki/judgels/jerahmeel/SessionProgress.java
@@ -3,6 +3,6 @@ package org.iatoki.judgels.jerahmeel;
 public enum SessionProgress {
     LOCKED,
     AVAILABLE,
-    ON_PROGRESS,
+    IN_PROGRESS,
     COMPLETED
 }
