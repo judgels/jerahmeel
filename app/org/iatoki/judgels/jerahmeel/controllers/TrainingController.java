28fee8b4ccebedd3905ee6c356f563d8913dde59
Ashar Fuadi <fushar@gmail.com>
2015-10-06 17:18:49 +0700

    Show all archives that are root's children on homepage

--- app/org/iatoki/judgels/jerahmeel/controllers/TrainingController.java
+++ app/org/iatoki/judgels/jerahmeel/controllers/TrainingController.java
@@ -1,10 +1,16 @@
 package org.iatoki.judgels.jerahmeel.controllers;
 
+import org.iatoki.judgels.jerahmeel.Archive;
+import org.iatoki.judgels.jerahmeel.ArchiveWithScore;
 import org.iatoki.judgels.jerahmeel.Curriculum;
+import org.iatoki.judgels.jerahmeel.JerahmeelUtils;
 import org.iatoki.judgels.jerahmeel.controllers.securities.Authenticated;
 import org.iatoki.judgels.jerahmeel.controllers.securities.GuestView;
+import org.iatoki.judgels.jerahmeel.services.ArchiveService;
 import org.iatoki.judgels.jerahmeel.services.CurriculumService;
 import org.iatoki.judgels.jerahmeel.views.html.training.listTrainingsView;
+import org.iatoki.judgels.jerahmeel.views.html.training.listTrainingsWithScoreView;
+import org.iatoki.judgels.play.IdentityUtils;
 import org.iatoki.judgels.play.LazyHtml;
 import org.iatoki.judgels.play.controllers.AbstractJudgelsController;
 import org.iatoki.judgels.play.views.html.layouts.headingLayout;
@@ -22,10 +28,12 @@ import java.util.List;
 public final class TrainingController extends AbstractJudgelsController {
 
     private final CurriculumService curriculumService;
+    private final ArchiveService archiveService;
 
     @Inject
-    public TrainingController(CurriculumService curriculumService) {
+    public TrainingController(CurriculumService curriculumService, ArchiveService archiveService) {
         this.curriculumService = curriculumService;
+        this.archiveService = archiveService;
     }
 
     @Authenticated(GuestView.class)
@@ -39,11 +47,19 @@ public final class TrainingController extends AbstractJudgelsController {
     public Result listTrainings() {
         List<Curriculum> curriculums = curriculumService.getAllCurriculums();
 
-        return showListTrainings(curriculums);
+        LazyHtml content;
+        if (JerahmeelUtils.isGuest()) {
+            List<Archive> archives = archiveService.getChildArchives("");
+            content = new LazyHtml(listTrainingsView.render(curriculums, archives));
+        } else {
+            List<ArchiveWithScore> archives = archiveService.getChildArchivesWithScore("", IdentityUtils.getUserJid());
+            content = new LazyHtml(listTrainingsWithScoreView.render(curriculums, archives));
+        }
+
+        return showListTrainings(content);
     }
 
-    private Result showListTrainings(List<Curriculum> curriculums) {
-        LazyHtml content = new LazyHtml(listTrainingsView.render(curriculums));
+    private Result showListTrainings(LazyHtml content) {
         content.appendLayout(c -> headingLayout.render(Messages.get("training.home"), c));
         JerahmeelControllerUtils.getInstance().appendSidebarLayout(content);
         JerahmeelControllerUtils.getInstance().appendBreadcrumbsLayout(content, TrainingControllerUtils.getBreadcrumbsBuilder().build());
